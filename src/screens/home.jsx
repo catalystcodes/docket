aa8130612371ffd7a4704be795bbc73609a915cf
catalystcodes <muh4mub@gmail.com>
2024-05-31 21:41:43 +0100

    feat: taskHeader

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, StyleSheet, Text, Image } from "react-native";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import InputField from "../components/molecules/InputField";
 
 const Home = () => {
   return (
@@ -18,6 +19,19 @@ const Home = () => {
           style={styles.img}
         />
       </View>
+      <InputField placeholder={"Find your task here...."} />
+      <Text style={styles.taskLabel}>Your Task</Text>
+      <View style={styles.taskProgress}>
+        <View style={styles.taskProgressStyle}>
+          <Text style={styles.text}>In Progress</Text>
+        </View>
+        <View style={styles.taskProgressStyle}>
+          <Text style={styles.text}>To Do</Text>
+        </View>
+        <View style={styles.taskProgressStyle}>
+          <Text style={styles.text}> Completed</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -26,6 +40,7 @@ const styles = StyleSheet.create({
   wholeDiv: {
     paddingLeft: wp(5.1),
     paddingRight: wp(5.1),
+    backgroundColor: "#fff",
   },
   avatarDetails: {
     display: "flex",
@@ -44,6 +59,32 @@ const styles = StyleSheet.create({
   avatarName: {
     fontWeight: "600",
     fontSize: 16,
+  },
+  taskLabel: {
+    fontWeight: "600",
+    fontSize: 16,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
+  taskProgress: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  taskProgressStyle: {
+    paddingTop: 9,
+    paddingBottom: 9,
+    paddingLeft: 19,
+    paddingRight: 19,
+    backgroundColor: "#0560FD",
+    borderRadius: 10,
+    // marginRight: 2,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "500",
   },
 });
 
