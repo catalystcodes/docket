b67a8b8edc8be5a46909d1c4f4a9a54034d7e4fc
catalystcodes <muh4mub@gmail.com>
2024-05-31 20:36:41 +0100

    feat: header

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -5,10 +5,18 @@ import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 const Home = () => {
   return (
     <View style={styles.wholeDiv}>
-      <View>
-        <View>
+      <View style={styles.avatarDetails}>
+        <View style={styles.subDetails1}>
           <Image source={require("../../src/assets/images/avater.png")} />
+          <View style={styles.subDetails}>
+            <Text>Hello</Text>
+            <Text>Sarthak</Text>
+          </View>
         </View>
+        <Image
+          source={require("../../src/assets/images/stroks.png")}
+          style={styles.img}
+        />
       </View>
     </View>
   );
@@ -17,6 +25,20 @@ const Home = () => {
 const styles = StyleSheet.create({
   wholeDiv: {
     paddingLeft: wp(5.1),
+    paddingRight: wp(5.1),
+  },
+  avatarDetails: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  subDetails1: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  subDetails: {
+    marginLeft: 3,
   },
 });
 
