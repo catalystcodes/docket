1aa5f8171b08cf1e5fb7fb424ed917ea9c78bee0
catalystcodes <muh4mub@gmail.com>
2025-01-12 14:19:56 +0100

    update data base

--- App.js
+++ App.js
@@ -8,21 +8,25 @@ import { NavigationContainer } from "@react-navigation/native";
 import AppRoutes from "./src/AppRoutes";
 import SignUp from "./src/screens/signUp";
 import AuthProvider from "./src/context";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="dark" />
-      <PaperProvider>
-        <AuthProvider>
-          <SafeAreaProvider>
-            <SafeAreaView style={{ flexGrow: 1 }}>
-              <AppRoutes />
-            </SafeAreaView>
-          </SafeAreaProvider>
-        </AuthProvider>
-      </PaperProvider>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <StatusBar style="dark" />
+        <PaperProvider>
+          <AuthProvider>
+            <SafeAreaProvider>
+              <SafeAreaView style={{ flexGrow: 1 }}>
+                <AppRoutes />
+              </SafeAreaView>
+            </SafeAreaProvider>
+          </AuthProvider>
+        </PaperProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
